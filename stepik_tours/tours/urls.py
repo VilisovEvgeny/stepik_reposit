6c7592e043778c1146d6fa0f99707b58b18868a4
VilisovEvgeny <jokersetru@gmail.com>
2022-03-04 22:18:36 +0500

    First upload project

--- stepik_tours/tours/urls.py
+++ stepik_tours/tours/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.main_view, name='home'),
     path('departure/<str:departure>', views.departure_view, name='departure'),
-    path('tour/<int:id>', views.tour_view, name='tour'),
+    path('tour/<int:tour_id>', views.tour_view, name='tour'),
 ]
--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render
+from .data import *
 
 
 def main_view(request):
     context = {
+        "title": title,
+        "subtitle": subtitle,
+        "description": description,
+        "departures": departures,
+        "tours": tours,
 
     }
     return render(request, 'tours/index.html', context=context)
 
 
-def departure_view(request):
+def departure_view(request, departure):
+    dep = departures[departure]
     context = {
+        "title": title,
+        "subtitle": subtitle,
+        "description": description,
+        "departures": departures,
+        "tours": tours,
+        "departure": departure,
+        "dep": dep
 
     }
     return render(request, 'tours/departure.html', context=context)
 
 
-def tour_view(request):
-    context = {
+def tour_view(request, tour_id):
 
+    context = {
+        "title": title,
+        "subtitle": subtitle,
+        "description": description,
+        "departures": departures,
+        "departure": departures[tours[tour_id]['departure']],
+        "tours": tours[tour_id],
+        "tour_id": tour_id,
+        "stars": int(tours[tour_id]['stars'])*'★'
     }
     return render(request, 'tours/tour.html', context=context)
